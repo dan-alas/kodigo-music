aee56cd3616db74cf30532f2231e3d86a6b6e39f
Daniel Alas <alasalexander76@gmail.com>
2025-06-29 21:17:49 -0600

    Configuracion de canciones mobile

--- src/components/songs/Cancion.jsx
+++ src/components/songs/Cancion.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Cancion({cancion}) {
     const { img, title, artist } = cancion;
   return (
-    <div className="card bg-transparent text-white border-0" style={{ minWidth: "200px", scrollSnapAlign: "start" }}>
-        <img src={img} className="card-img-top" height="200" alt="Cancion"/>
+    <div className="card bg-transparent text-white border-0 cancion">
+        <img src={img} className="card-img-top img-card" alt="Cancion"/>
 
         <div className="card-body">
-            <h5 className="card-title fs-5">{title}</h5>
+            <h5 className="card-title fs-6 fs-md-5">{title}</h5>
             <p className="card-text">{artist}</p>
         </div>
         
